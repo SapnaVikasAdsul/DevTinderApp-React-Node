3d76195b15feb67e6372044e3b7b0bc2bb6a481f
sapna-adsul <sapnaadsul15@gmail.com>
2026-07-04 19:03:31 +0530

    schema validations

--- devTInder-api/src/models/user.js
+++ devTInder-api/src/models/user.js
@@ -1,23 +1,53 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 20,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      lowerCase: true,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      min: 18,
+      max: 60,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "femaile", "others"].includes(value)) {
+          throw new Error("Gender not valid"); //not for existing user butworks for new users
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+    },
+    about: {
+      type: String,
+      default: "This is default value",
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
+  {
+    timestamps: true,
   },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+);
 module.exports = mongoose.model("User", userSchema);
