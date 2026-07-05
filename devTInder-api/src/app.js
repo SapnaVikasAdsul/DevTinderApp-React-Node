f9cbf6b4f607a46a940b7864883655bc35d4f298
sapna-adsul <sapnaadsul15@gmail.com>
2026-07-05 23:28:24 +0530

    Authentication and JWT and  Cookies

--- devTInder-api/src/app.js
+++ devTInder-api/src/app.js
@@ -2,11 +2,16 @@ console.log("starting backend");
 
 const express = require("express");
 const connectToMongoDB = require("./config/database");
+const bcrypt = require("bcrypt");
 const app = express();
 const User = require("./models/user");
-const validateSignUpData = require("./utils/validateSignUpData");
-app.use(express.json()); //middleware to convert json response
+const { validateSignUpData } = require("./utils/validateSignUpData");
+const jwt = require("jsonwebtoken");
+const { userAuth } = require("./middlewares/auth");
+const cookieParser = require("cookie-parser")
 
+app.use(express.json()); //middleware to convert json response
+app.use(cookieParser())
 app.post("/signup", async (req, res) => {
   //     const userObj = {
   //     firstName: "Sapna",
@@ -21,15 +26,44 @@ app.post("/signup", async (req, res) => {
     //validation of data
     validateSignUpData(req);
 
+    const { password } = req.body;
     //Encrypt the password
-
-    
-
-    const user = new User(req.body);
+    const passwordHash = await bcrypt.hash(password, 10);
+    const userObj = { ...req.body, password: passwordHash };
+    const user = new User(userObj);
     await user.save();
     res.send("User added successfully");
   } catch (err) {
     res.status(400).send("Error saving user:" + err.message);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { emailId, password } = req.body;
+  const user = await User.findOne({ emailId: emailId });
+  console.log(user);
+  if (!user) {
+    throw new Error("email id not exists");
+  }
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (isPasswordValid) {
+    //Create a JWT token
+    const token = await jwt.sign({ _id: user._id }, "DEV@tinder48598");
+  
+    res.cookie("token", token);
+
+    res.send("Login successful!");
+  } else {
+    res.send("Password incorrect");
+  }
+});
+
+app.get("/profile", userAuth, async (req, res) => {
+  try {
+    const user = req.user
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("SOmething went wrong");
   }
 });
 
@@ -82,14 +116,17 @@ app.patch("/user/:userId", async (req, res) => {
     if (data?.skills.length > 5) {
       throw new Error("Skills cannot be more than 5.");
     }
-    const user = await User.findByIdAndUpdate(userId, data);
+    // Hash password if being updated
+    if (data.password) {
+      data.password = await hashPassword(data.password);
+    }
+    const user = await User.findByIdAndUpdate(userId, data, { new: true });
     res.send("user updated successfully");
   } catch (err) {
     res.status(400).send("something went wrong");
   }
 });
 
-app.put("/user", async (req, res) => {});
 connectToMongoDB()
   .then(() => {
     console.log("database connection established");
---
+++ devTInder-api/src/middlewares/auth.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+const { Error } = require("mongoose");
+const User = require("../models/user")
+const cookieParser = require("cookie-parser")
+const userAuth = async (req, res, next) => {
+   try {
+      const { token } = req.cookies;
+      if (!token) {
+         throw new Error("token is not valid");
+      }
+      const decodeObj = await jwt.verify(token, "DEV@tinder48598") //token + secret key
+      const { _id } = decodeObj
+      console.log(_id)
+      const user = await User.findById(_id);
+      console.log("from auth" + user)
+      if (!user) {
+         throw new Error("User not found")
+      }
+      req.user = user
+      next();//to move to next request handler
+   }
+   catch (err) {
+      res.status(400).send("error: " + err.message)
+   }
+
+}
+
+module.exports = { userAuth }
