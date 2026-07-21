dff0ac93234073f00975f141c924de5858f1c535
sapna-adsul <sapnaadsul15@gmail.com>
2026-07-21 12:06:08 +0530

    redirect to login if not authenticatd

--- devTInder-api/src/middlewares/auth.js
+++ devTInder-api/src/middlewares/auth.js
@@ -6,7 +6,7 @@ const userAuth = async (req, res, next) => {
    try {
       const { token } = req.cookies;
       if (!token) {
-         throw new Error("token is not valid");
+         return res.status(401).send("please login!")
       }
       const decodeObj = await jwt.verify(token, "DEV@tinder48598") //token + secret key
       const { _id } = decodeObj
--- devTinder-web/src/Body.jsx
+++ devTinder-web/src/Body.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import Navbar from './Navbar'
-import { Outlet } from 'react-router-dom'
+import { Navigate, Outlet, useNavigate } from 'react-router-dom'
 import Footer from './Footer'
+import { useDispatch, useSelector } from 'react-redux'
+import { addUser } from './utils/userSlice'
+import axios from 'axios'
+import { useEffect } from 'react'
 
 function Body() {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const userData=useSelector((store)=>store.data)
+    const fetchUser = async () => {
+        try {
+            const user = await axios.get("http://localhost:3000/profile/view", { withCredentials: true })
+            dispatch(addUser(user.data))
+        }
+
+        catch (err) {
+            if (err.status === 401) {
+                navigate("/login")
+            }
+            console.error(err);
+        }
+    }
+    useEffect(() => {
+        if(!userData){
+        fetchUser()
+        }
+    }, [])
     return (
         <div>
             <Navbar />
